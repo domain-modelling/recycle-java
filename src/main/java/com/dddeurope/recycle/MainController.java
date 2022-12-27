5b66049fbdabdf016f67e721d80601df57e09fd0
Michel Grootjans <michel.grootjans@gmail.com>
2022-12-27 10:50:46 +0100

    cleanup

--- src/main/java/com/dddeurope/recycle/MainController.java
+++ src/main/java/com/dddeurope/recycle/MainController.java
@@ -8,17 +8,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
-
 @RestController
 public class MainController {
 
-    private final ObjectMapper objectMapper;
+    private final ObjectMapper mapper;
 
     public MainController() {
-        objectMapper = new ObjectMapper();
-        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-        objectMapper.registerModule(new JavaTimeModule());
+        mapper = new ObjectMapper();
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        mapper.registerModule(new JavaTimeModule());
     }
 
     @GetMapping("/validate")
@@ -28,35 +26,8 @@ public class MainController {
 
     @PostMapping("/handle-command")
     public String handle() throws JsonProcessingException {
-        Event<PriceWasCalculated> response = new Event<>(new PriceWasCalculated("Tom", BigDecimal.valueOf(0), "EUR"));
-        return objectMapper.writeValueAsString(response);
-
-//        return toJson(
-//            record("type", "PriceWasCalculated"),
-//            record("created_at", LocalDateTime.now()),
-//            record("event_id", UUID.randomUUID()),
-//            record("payload", toJson(
-//                record("person_id", "Tom"),
-//                record("price_amount", BigDecimal.ZERO),
-//                record("price_currency", "EUR")
-//            ))
-//        );
-    }
-
-    private static String toJson(String... values) {
-        return "{" + String.join(", ", values) + "}";
-    }
-
-    private static String record(String key, Object value) {
-        return enquote(key) + ": " + enquote(value.toString());
-    }
-
-    private static String record(String key, BigDecimal value) {
-        return enquote(key) + ": " + value;
-    }
-
-    private static String enquote(String value) {
-        if (value.startsWith("{")) return value;
-        return "\"" + value + "\"";
+        return mapper.writeValueAsString(
+            new Event<>(new PriceWasCalculated("Tom", 0.0, "EUR"))
+        );
     }
 }
--- src/main/java/com/dddeurope/recycle/PriceWasCalculated.java
+++ src/main/java/com/dddeurope/recycle/PriceWasCalculated.java
@@ -2,19 +2,17 @@ package com.dddeurope.recycle;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import java.math.BigDecimal;
-
 public class PriceWasCalculated {
     @JsonProperty("person_id")
     public final String personId;
 
     @JsonProperty("price_amount")
-    public final BigDecimal amount;
+    public final double amount;
 
     @JsonProperty("price_currency")
     public final String currency;
 
-    public PriceWasCalculated(String personId, BigDecimal amount, String currency) {
+    public PriceWasCalculated(String personId, double amount, String currency) {
         this.personId = personId;
         this.amount = amount;
         this.currency = currency;
