98d5c0c7d11d443dc25825843e725fba23212285
Michel Grootjans <michel.grootjans@gmail.com>
2022-12-27 09:00:08 +0100

    initial commit

---
+++ src/main/java/com/dddeurope/recycle/RecycleJavaApplication.java
@@ -0,0 +1,13 @@
+package com.dddeurope.recycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecycleJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecycleJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dddeurope/recycle/RecycleJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dddeurope.recycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecycleJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
