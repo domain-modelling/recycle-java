cd228f5125e750805906e47bbd2d41f15cc2c682
Michel Grootjans <michel.grootjans@gmail.com>
2022-12-27 11:01:36 +0100

    cleanup

--- src/main/java/com/dddeurope/recycle/Event.java
+++ src/main/java/com/dddeurope/recycle/Event.java
@@ -5,23 +5,18 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-public class Event<TPayload> {
-    @JsonProperty("type")
-    public final String type;
-
-    @JsonProperty("event_id")
-    public final String eventId;
-
-    @JsonProperty("created_at")
-    public final LocalDateTime createdAt;
-
-    @JsonProperty("payload")
-    public final TPayload payload;
-
+public record Event<TPayload>(
+    @JsonProperty("type") String type,
+    @JsonProperty("event_id") String eventId,
+    @JsonProperty("created_at") LocalDateTime createdAt,
+    @JsonProperty("payload") TPayload payload
+) {
     public Event(TPayload payload) {
-        this.type = payload.getClass().getSimpleName();
-        this.eventId = UUID.randomUUID().toString();
-        this.createdAt = LocalDateTime.now();
-        this.payload = payload;
+        this(
+            payload.getClass().getSimpleName(),
+            UUID.randomUUID().toString(),
+            LocalDateTime.now(),
+            payload
+        );
     }
 }
--- src/main/java/com/dddeurope/recycle/PriceWasCalculated.java
+++ src/main/java/com/dddeurope/recycle/PriceWasCalculated.java
@@ -2,19 +2,8 @@ package com.dddeurope.recycle;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class PriceWasCalculated {
-    @JsonProperty("person_id")
-    public final String personId;
-
-    @JsonProperty("price_amount")
-    public final double amount;
-
-    @JsonProperty("price_currency")
-    public final String currency;
-
-    public PriceWasCalculated(String personId, double amount, String currency) {
-        this.personId = personId;
-        this.amount = amount;
-        this.currency = currency;
-    }
-}
+public record PriceWasCalculated(
+    @JsonProperty("person_id") String personId,
+    @JsonProperty("price_amount") double amount,
+    @JsonProperty("price_currency") String currency
+) { }
