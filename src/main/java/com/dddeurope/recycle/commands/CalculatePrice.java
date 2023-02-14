87e8d81b8a03ed8961af180caf178c4dae8a1351
Michel Grootjans <michel.grootjans@gmail.com>
2023-02-14 19:24:04 +0100

    Convert CalculatePrice to record type

--- src/main/java/com/dddeurope/recycle/commands/CalculatePrice.java
+++ src/main/java/com/dddeurope/recycle/commands/CalculatePrice.java
@@ -2,44 +2,4 @@ package com.dddeurope.recycle.commands;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import java.util.Objects;
-
-public final class CalculatePrice implements Command {
-    @JsonProperty("card_id")
-    private String cardId;
-
-    public CalculatePrice() {
-    }
-
-    public CalculatePrice(String cardId) {
-        this.cardId = cardId;
-    }
-
-    public String getCardId() {
-        return cardId;
-    }
-
-    public void setCardId(String cardId) {
-        this.cardId = cardId;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) return true;
-        if (obj == null || obj.getClass() != this.getClass()) return false;
-        var that = (CalculatePrice) obj;
-        return Objects.equals(this.cardId, that.cardId);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(cardId);
-    }
-
-    @Override
-    public String toString() {
-        return "CalculatePrice[" +
-                "cardId=" + cardId + ']';
-    }
-
-}
+public record CalculatePrice (@JsonProperty("card_id") String cardId) implements Command { }
